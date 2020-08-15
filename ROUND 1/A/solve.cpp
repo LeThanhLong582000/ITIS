4bd1558c62479854aefb5e8afc67992b727f8e1d
Huy Tran <viethuy99lc@gmail.com>
2020-08-15 11:44:31 +0700

    problem A

--- ROUND 1/A/solve.cpp
+++ ROUND 1/A/solve.cpp
@@ -1,38 +1,78 @@
-#include<bits/stdc++.h>
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+
 using namespace std;
 
-vector<int>ans;
-void sortArray(vector<int> arr, vector<int> other)
-{
-    vector<int>newone;
-    sort(other.begin(), other.end());
-    for(int i = 0; i < arr.size(); i++){
-        if(!binary_search(other.begin(), other.end(), arr[i])){
-            newone.push_back(arr[i]);
-        }
+#define FTB(i,a,b) for(LL i=a;i<=b;i++)
+#define FT(i,a,b) for(LL i=a;i<b;i++)
+#define FGB(i,a,b) for(LL i=a;i>=b;i--)
+#define FG(i,a,b) for(LL i=a;i>b;i--)
+#define endl "\n"
+#define PB push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define debug cout<<"VL\n";
+#define PI 3.1415926535897
+#define lbnd(dau,cuoi,giatri) lower_bound(dau,cuoi,giatri)
+#define ubnd(dau,cuoi,giatri) upper_bound(dau,cuoi,giatri)
+typedef double DB;
+typedef long long LL;
+typedef unsigned long long UL;
+typedef long double LD;
+typedef pair<LL, LL> II;
+typedef vector<LL> VI;
+typedef vector<bool> VB;
+
+
+LL n, x, nho, to, res;
+
+
+LL c(LL n, LL k) {
+    LL res = 1;
+    FTB(i, 1, k) {
+        res *= n - i + 1;
+        res /= i;
     }
-    sort(newone.begin(), newone.end());
-    int i = 0, j = 0, sptu = 0;
-    while(sptu != arr.size()){
-        if(binary_search(other.begin(), other.end(), arr[i])){
-            ans.push_back(arr[i]);
-            i++;
-            continue;
-        } 
-        else{
-            ans.push_back(newone[j]);
-            j++; i++;
-        }
-        sptu++;
-    }
-    //ans.resize(arr.size());
-    //return ans;
+    return res;
 }
-main(){
-	vector<int>arr{2,3,1,43,5,3,5,2,1,1,1};
-	vector<int>other{2,1};
-	sortArray(arr, other);
-	for(int i = 0; i < ans.size(); i++){
-		cout << ans[i] << " ";
-	}
+
+
+int main() {
+  freopen("0.in", "r", stdin);
+  freopen("0.out", "w", stdout);
+    cin >> n;
+    FTB(i, 1, n) {
+        cin >> x;
+        if (x == 1) nho++;
+        else to++;
+    }
+    LL hvi[] = {30, 3, 1, 1};
+    FTB(i, 0, 3) {
+        if (i > to) continue;
+        if (n - i < 6 - 2 * i) continue;
+        res += hvi[i] * c(to, i) * c(n - i, 6 - 2 * i);
+    }
+    cout << res;
+
+
+
+
+    return 0;
 }
