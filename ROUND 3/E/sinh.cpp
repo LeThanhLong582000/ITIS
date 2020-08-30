128e3bd892828ce4472a75a44b3d8ccd4f2baaf2
Minh Duc <p.m.d.2203@gmail.com>
2020-08-30 11:56:40 +0700

    fixed solution E

--- ROUND 3/E/sinh.cpp
+++ ROUND 3/E/sinh.cpp
@@ -12,8 +12,8 @@ void sinhInput(int Begin,int numberTest) {
 		out << t << endl;
 		while(t--){
 			ll n, m;
-			n = (ll)rand() * (ll)rand() * (ll)rand() % 100000+1;
-			m = (ll)rand() * (ll)rand() * (ll)rand() % 100000+1;
+			n = (ll)rand() * (ll)rand() * (ll)rand() % 10000+1;
+			m = (ll)rand() * (ll)rand() * (ll)rand() % 10000+1;
 			if(n == 0) n++;
 			if(m == 0) m++;
 			if(n < 0) n *= -1;
@@ -35,13 +35,13 @@ void sinhOutput(int Begin,int numberTest) {
 		int t;
 		in >> t;
     	while(t--){
-      	  	int n, m;
+      	  	long long  n, m;
        	 	in >> n >> m;
-       	 	int ans = (n * m - 1) * n * m;
+       	 	long long ans = 1LL*(n * m - 1) * n * m;
         	if(n >= 1 && m >= 2)
-            	ans -= (4 * (n - 1) * (m - 2));
-        	if(n >=2 && m >=1 )
-            	ans -= (4 * (n - 2) * (m - 1));
+            	ans -= (1LL* 4 * (n - 1) * (m - 2));
+        	if(n >= 2 && m >=1 )
+            	ans -= (1LL* 4 * (n - 2) * (m - 1));
         	out << ans << endl;
     }
 	}
