5203a78b0bea2234cb866a9afa52fdda29b2f339
A the a <nguyenthanhlam1672000@gmail.com>
2020-09-05 00:15:40 +0700

    problem E added

---
+++ ROUND 4/E/sol.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> ii;
+
+int main() {
+  	int n, m;
+  	cin >> n >> m;
+  	char A[n][m];
+  	for (int r = 0; r < n; r++) {
+    	for (int c = 0; c < m; c++) {
+      		cin >> A[r][c];
+		}
+  	}
+  	int br = -1, bc = -1;
+  	int len = 0;
+  	for (int r = 0; r < n; r++) {
+    	for (int c = 0; c < m; c++) {
+      		if (A[r][c] == 'B') {
+        		if (br == -1 and bc == -1) {
+          			br = r;
+          			bc = c;
+        		}
+        		len++;
+      		}
+    	}
+    	if (br != -1 and bc != -1) {
+      		break;
+    	}
+  	}
+  	br += len / 2 + 1;
+  	bc += len / 2 + 1;
+  	cout << br << " " << bc << endl;
+  	return 0;
+}
